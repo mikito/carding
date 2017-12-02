e7e31447e2b592bd1c9f8ca578ff5d2b09b6c94a
Mikito Yoshiya <mikito.yoshiya@tanoshim.co.jp>
2017-12-02 20:18:29 +0900

    erb

--- card.rb
+++ card.rb
@@ -1,5 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
+require 'erb'
+require 'fileutils'
 
 url = 'https://cookpad.com/recipe/2069863'
 charset = nil
@@ -12,18 +14,18 @@ doc = Nokogiri::HTML.parse(html, nil, charset)
 
 model = {}
 
-model[:title] = doc.title.gsub("【クックパッド】 簡単おいしいみんなのレシピが279万品", "")
+model["title"] = doc.title.gsub("【クックパッド】 簡単おいしいみんなのレシピが279万品", "")
 
 
-model[:img] = doc.xpath('//div[@id="main-photo"]/img').attribute('src').value
+model["image"] = doc.xpath('//div[@id="main-photo"]/img').attribute('src').value
 
-model[:ingredients] = []
+model["ingredients"] = []
 
 doc.xpath('//div[contains(@class,"ingredient_row")]').each do |node|
   ingredient = {} 
-  ingredient[:name] = node.xpath('div[@class="ingredient_name"]').inner_text
-  ingredient[:auantity] = node.xpath('div[@class="ingredient_quantity amount"]').inner_text
-  model[:ingredients] << ingredient
+  ingredient["name"] = node.xpath('div[@class="ingredient_name"]').inner_text.strip
+  ingredient["quantity"] = node.xpath('div[@class="ingredient_quantity amount"]').inner_text.strip
+  model["ingredients"] << ingredient
 end
 
 #doc.xpath('//dd[@class="instruction"]').each do |node|
@@ -31,3 +33,9 @@ end
 #end
 
 p model
+
+template = File.read("card.erb")
+
+File.open("card.html", mode = "w:utf-8") do |f|
+  f.write(ERB.new(template, nil, '-').result(binding))
+end
